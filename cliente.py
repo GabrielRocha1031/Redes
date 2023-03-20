add2706d37cfb2c163ecaf8d15f343f5641658b7
GabrielRocha1031 <54642896+GabrielRocha1031@users.noreply.github.com>
2023-03-19 21:28:13 -0300

    Add files via upload
    jogo pronto

--- cliente.py
+++ cliente.py
@@ -10,24 +10,21 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Conecta o socket ao servidor
 client_socket.connect((HOST, PORT))
 
-
-
 # Loop para enviar mensagens ao servidor
 while True:
-
     # Recebe a resposta do servidor
     data = client_socket.recv(1024)
     
     # Imprime a resposta do servidor
-    print(f"{data.decode()}")
+    print(f"Resposta do servidor: {data.decode()}")
 
-
+    if data.decode() == '0':
+        break
     # Lê uma mensagem do usuário
-    message = input(": ")
+    name = input("Digite: ")
     
     # Envia a mensagem para o servidor
-    client_socket.send(message.encode())
+    client_socket.send(name.encode())
     
-
 # Encerra a conexão com o servidor
 client_socket.close()
